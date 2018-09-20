2c3e82d0dd3b69338a65840dbd3ecece97c8216c
joalorro <joalorro@gmail.com>
2018-09-20 19:37:35 -0400

    Done.

--- src/Controls.js
+++ src/Controls.js
@@ -2,18 +2,18 @@ import React, { PureComponent } from 'react';
 
 class Controls extends PureComponent {
 
-  render() {
-    return (
-      <div>
-        <div>
-          <button type="text" onClick={() => this.props.updateIntervalSetting(-1)}>-</button>
-          {this.props.updateInterval}
-          <button type="text" onClick={() => this.props.updateIntervalSetting(1)}>+</button>
-        </div>
-        <button onClick={this.props.handleAddTimer}>Add New Timer</button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div>
+					<button type="text" onClick={() => this.props.updateIntervalSetting(-1)}>-</button>
+					{this.props.updateInterval}
+					<button type="text" onClick={() => this.props.updateIntervalSetting(1)}>+</button>
+				</div>
+				<button onClick={this.props.handleAddTimer}>Add New Timer</button>
+			</div>
+		);
+	}
 
 
 }
